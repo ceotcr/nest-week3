f1d8b99e1ab4f2054ceddc0aa7a01f7634d7f472
Chetan (TCR) Sapkal <68210482+ceotcr@users.noreply.github.com>
2025-04-25 11:00:36 +0530

    Fix logger middleware to use originalUrl instead of url for accurate logging

--- task-mngr/src/common/middlewares/logger.middleware.ts
+++ task-mngr/src/common/middlewares/logger.middleware.ts
@@ -3,7 +3,7 @@ import { NextFunction, Request, Response } from 'express';
 export const loggerMiddleware = (req: Request, res: Response, next: NextFunction) => {
     console.log(JSON.stringify({
         method: req.method,
-        url: req.url,
+        url: req.originalUrl,
         timeStamp: new Date().toLocaleString()
     }))
     next();
